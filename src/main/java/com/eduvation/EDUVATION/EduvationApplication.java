341addef28c1d502a0cf91ac3a1c0146fc808992
MUKESH <mukesh19069@mech.ssn.edu.in>
2022-09-21 14:54:05 +0530

    first commit

---
+++ src/main/java/com/eduvation/EDUVATION/EduvationApplication.java
@@ -0,0 +1,13 @@
+package com.eduvation.EDUVATION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduvationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduvationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduvation/EDUVATION/EduvationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduvation.EDUVATION;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduvationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
